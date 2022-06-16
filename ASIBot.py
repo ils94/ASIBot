1c70b11e50872f9d8159106fa25e2cb723e047f6
Igor Lima <igorlimaesilva94@gmail.com>
2022-06-16 11:25:17 -0300

    Mudado a forma como o bot é iniciado para deixar ainda mais pratico.

--- ASIBot.py
+++ ASIBot.py
@@ -1,7 +1,8 @@
 import re
-from tkinter import Tk, Button, Text, Label, END
+from tkinter import Tk, Text, Label, END
 import threading
 import time
+import keyboard
 import pyautogui
 
 patrimonio_list = []
@@ -11,12 +12,8 @@ def multithreading(funcao):
     threading.Thread(target=funcao).start()
 
 
-def iniciar_bot():
+def bot():
     global patrimonio_list
-
-    contador = 3
-
-    iniciar["state"] = "disabled"
 
     filtrar = re.sub("[^0-9\n\\s]", "", patrimonios.get("1.0", END)).replace(" ", "\n")
 
@@ -29,12 +26,7 @@ def iniciar_bot():
     patrimonio_list = patrimonios.get("1.0", END).split("\n")
     patrimonio_list.remove("")
 
-    while contador > 0:
-        contagem_regressiva["text"] = "Bot iniciando em: " + str(contador)
-        contador = contador - 1
-        time.sleep(1)
-
-    contagem_regressiva["text"] = "Bot Iniciado."
+    aviso.config(text="Bot Iniciado.")
 
     for index in patrimonio_list:
 
@@ -44,8 +36,15 @@ def iniciar_bot():
         pyautogui.typewrite(index)
         pyautogui.press("Tab")
 
-    iniciar["state"] = "normal"
-    contagem_regressiva["text"] = "Operação Concluída."
+    aviso.config(text="Operação Concluída.")
+
+    patrimonios.delete("1.0", END)
+
+
+def iniciar_bot(event):
+    if keyboard.is_pressed("shift"):
+        time.sleep(2)
+        multithreading(bot)
 
 
 root = Tk()
@@ -65,13 +64,11 @@ root.iconbitmap("icones/bot.ico")
 root.resizable(False, False)
 root.attributes("-topmost", True)
 
-patrimonios = Text(root, height=10)
+patrimonios = Text(root, height=13)
 patrimonios.pack(padx=5, pady=5)
 
-iniciar = Button(root, text="Iniciar", width=10, height=1, command=lambda: multithreading(iniciar_bot))
-iniciar.pack(pady=5)
+aviso = Label(root, text="...", width=20, height=1)
+aviso.pack(pady=5)
 
-contagem_regressiva = Label(root, text="Preencha o campo a cima\ne click em Iniciar.", width=20, height=2)
-contagem_regressiva.pack(pady=5)
-
+root.bind("<FocusOut>", iniciar_bot)
 root.mainloop()
